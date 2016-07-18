a5c3456cb517f7f983bedb5e75c5afd2baeb7815
Adrian Kothman <adrian@kothman.io>
2016-07-18 18:43:48 +0000

    Fix formatting

--- src/CSVParser.php
+++ src/CSVParser.php
@@ -7,7 +7,7 @@ class CSVParser
 
     protected $resource = null;
     protected $options = null;
-
+    
     /**
      * Create a new CSVParser instance
      *
@@ -18,136 +18,136 @@ class CSVParser
      * @param string $escape
      */
     public function __construct($path,
-    	    	     		$delimiter = null,
-			 	$length = null,
-				$enclosure = null,
-				$escape = null)
+                                $delimiter = null,
+                                $length = null,
+                                $enclosure = null,
+                                $escape = null)
     {
         if (!$this->load($path)) {
-	    throw new Exception('Unable to load resource: ' . $path);
+            throw new Exception('Unable to load resource: ' . $path);
         }
-	$this->options = new \Kothman\CSVParserOptions($delimiter, $length, $enclosure, $escape);
+        $this->options = new \Kothman\CSVParserOptions($delimiter, $length, $enclosure, $escape);
     }
-
+    
     /**
      * Load a resource to be used by the parser
      *
      * @param string $path A URL or file.
      * @return bool Indicate success or failiure
      */ 
-     private function load($path)
-     {
-	$this->resource = fopen($path, 'r');
-	return $this->resource?true:false;
-     }
-
+    private function load($path)
+    {
+        $this->resource = fopen($path, 'r');
+        return $this->resource?true:false;
+    }
+    
     /**
      * Called at the begining of any function that uses $resource
      *
      * @return void
      */
-     protected function check() {
-         if(!$this->resource) {
-	     throw new Exception('The CSVParser does not have a loaded resource.');
-	 }
-     }
-
+    protected function check() {
+        if(!$this->resource) {
+            throw new Exception('The CSVParser does not have a loaded resource.');
+        }
+    }
+    
     /**
      * Return the next row of the CSV file
      *
      * @return array | false
      */
-     public function row() {
-         $this->check();
-	 $data = fgetcsv($this->resource,
-			 $this->options->length,
-			 $this->options->delimiter,
-			 $this->options->enclosure,
-			 $this->options->escape);
-	 return $data;
-     }
-
+    public function row() {
+        $this->check();
+        $data = fgetcsv($this->resource,
+                        $this->options->length,
+                        $this->options->delimiter,
+                        $this->options->enclosure,
+                        $this->options->escape);
+        return $data;
+    }
+    
     /**
      * Resets the file position pointer back to the beginning of the file
      *
      * @return void
      */
-     public function rewind() {
-         $this->check();
-         fseek($this->resource, 0);
-     }
-
+    public function rewind() {
+        $this->check();
+        fseek($this->resource, 0);
+    }
+    
     /**
      * Returns the number of rows in the CSV resource by iterating through each row
      *
      * @return int Number of rows
      */
-     public function countRows() {
-     	 $this->check();
-
-         // Get current position, since we need to rewind the file
-	 $pos = ftell($this->resource);
-
-	 $rows = 0;
-	 $this->rewind();
-	 while($this->row()) {
-	     $rows++;
-	 }
-
-	 // Restore original position
-	 fseek($this->resource, $pos);
-
-	 return $rows;
-     }
-
+    public function countRows() {
+        $this->check();
+        
+        // Get current position, since we need to rewind the file
+        $pos = ftell($this->resource);
+        
+        $rows = 0;
+        $this->rewind();
+        while($this->row()) {
+            $rows++;
+        }
+        
+        // Restore original position
+        fseek($this->resource, $pos);
+        
+        return $rows;
+    }
+    
     /**
      * Count the number of columns based on the header row
      *
      * @return int Number of columns
      */
-     public function countColumns() {
-         $this->check();
-	 $pos = ftell($this->resource);
-	 
-	 $this->rewind();
-	 $row = $this->row();
-	 fseek($this->resource, $pos);
-
-	 return count($row);
-     }
-
+    public function countColumns() {
+        $this->check();
+        $pos = ftell($this->resource);
+        
+        $this->rewind();
+        $row = $this->row();
+        fseek($this->resource, $pos);
+        
+        return count($row);
+    }
+    
     /**
      * Convert the CSV resource to an associative array with the header row as keys
      *
      * @return array Associative array
      */
-     public function toDictionary()
-     {
-         $this->check();
-	 $pos = ftell($this->resource);
-
-	 $this->rewind();
-	 
-	 // first row is the header
-	 $header = $this->row();
-	 $rows = [];
-	 while($r = $this->row()) {
-	     $rows[] = array_combine($header, $r);
-	 }
-
-	 fseek($this->resource, $pos);
-	 
-	 return $rows;
-     }
-
+    public function toDictionary()
+    {
+        $this->check();
+        $pos = ftell($this->resource);
+        
+        $this->rewind();
+        
+        // first row is the header
+        $header = $this->row();
+        $rows = [];
+        while($r = $this->row()) {
+            $rows[] = array_combine($header, $r);
+        }
+        
+        fseek($this->resource, $pos);
+        
+        return $rows;
+    }
+    
     /**
      * Release loaded resources when no longer needed
      */
-     function __destruct()
-     {
+    function __destruct()
+    {
      	if($this->resource) {
-	    fclose($this->resource);
-	}
-     }
-
+            fclose($this->resource);
+        }
+    }
+    
 }
--- src/CSVParserOptions.php
+++ src/CSVParserOptions.php
@@ -4,21 +4,21 @@ namespace Kothman;
 
 class CSVParserOptions
 {
-
+    
     public $delimiter = ',';
     public $length = 0;
     public $enclosure = '"';
     public $escape = '\\';
-
+    
     public function __construct($delimiter = null,
                                 $length = null,
-				$enclosure = null,
-				$escape = null)
+                                $enclosure = null,
+                                $escape = null)
     {
         $this->delimiter = $delimiter?$delimiter:$this->delimiter;
-	$this->length = $length?$length:$this->length;
-	$this->enclosure = $enclosure?$enclosure:$this->enclosure;
-	$this->escape = $escape?$escape:$this->escape;
+        $this->length = $length?$length:$this->length;
+        $this->enclosure = $enclosure?$enclosure:$this->enclosure;
+        $this->escape = $escape?$escape:$this->escape;
     }
-
+    
 }
